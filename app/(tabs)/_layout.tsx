7a1e9aa91b61fbb1989d82a776bef49258e940bf
simo <simo@simos-MacBook-Pro.local>
2024-01-07 14:20:12 +0100

    initialCommit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,55 +1,60 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-
-import Colors from '../../constants/Colors';
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors';
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons'; 
+import {Ionicons} from '@expo/vector-icons';
+const Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
-      <Tabs.Screen
-        name="index"
+    <Tabs screenOptions={{
+        tabBarActiveTintColor : Colors.primary,
+        tabBarLabelStyle : {
+            fontFamily : 'mon-sb'
+        }
+    }}>
+        <Tabs.Screen 
+        name="index" 
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
+        tabBarLabel: 'Explore', 
+        tabBarIcon : ({color , size}) =>
+        <Ionicons name="search" color={color} size={size} />       
+    }} 
+       />
+
+       {/* Trips */}
+
+       <Tabs.Screen 
+        name="trips" 
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+        tabBarLabel: 'Trips', 
+        tabBarIcon : ({color , size}) =>
+        <FontAwesome5 name="airbnb" color={color} size={size} />       
+    }} 
+       />   
+
+       {/*  Inbox*/}
+
+       <Tabs.Screen 
+        name="inbox" 
+        options={{
+        tabBarLabel: 'Inbox', 
+        tabBarIcon : ({color , size}) =>
+        <MaterialCommunityIcons name="message-outline" color={color} size={size} />       
+    }} 
+       />
+
+       {/* Profile */}
+
+       <Tabs.Screen 
+        name="profile" 
+        options={{
+        tabBarLabel: 'Profile', 
+        tabBarIcon : ({color , size}) =>
+        <Ionicons name="person-circle-outline" color={color} size={size} />       
+    }} 
+       />
     </Tabs>
   );
-}
+};
+
+export default Layout;
